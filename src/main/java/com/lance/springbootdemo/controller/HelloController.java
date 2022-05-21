f720be047755e151d2b6849033bdf2cb37af8438
“lance” <lancey2030@gmail.com>
2022-05-22 00:51:44 +0800

    modify 1

--- src/main/java/com/lance/springbootdemo/controller/HelloController.java
+++ src/main/java/com/lance/springbootdemo/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @RequestMapping("/")
     @ResponseBody
     public String getHello(){
-        int s = 1;
+        int s = 2;
 
         return "+ hello";
     }
